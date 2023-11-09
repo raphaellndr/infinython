0abef0823cdf3140a2a0c16c5d8a9d43def298bc
rlandure <raphael.landure@visiblepatient.com>
2023-11-09 09:54:11 +0100

    Update init file

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,7 +1,4 @@
-"""{{cookiecutter.project_name|capitalize}} package root."""
+"""{{cookiecutter.project_name}} package root."""
 
-# The current version of this project is computed during the installation stage and then set in this
-# variable. Thus, the default value `0.0.0` is only here to create the `version` placeholder and
-# doesn't reflect the current version of this project.
 __version__ = "0.0.0"
 PROGRAM_NAME = "{{cookiecutter.project_name}}"
